d057a48032c28c8d8c076e0c3b5bbe0587af7dd7
vile <111662603+vile@users.noreply.github.com>
2024-06-27 23:15:31 -0400

    test: mock

---
+++ test/mocks/MockERC20.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import {ERC20} from "@solady/tokens/ERC20.sol";
+
+contract MockERC20 is ERC20 {
+    function mint(uint256 amount) external {
+        _mint(msg.sender, amount);
+    }
+
+    function name() public view override returns (string memory) {
+        return "Mock ERC20 Token";
+    }
+
+    /// @dev Returns the symbol of the token.
+    function symbol() public view override returns (string memory) {
+        return "MOCK";
+    }
+
+    /// @dev Returns the decimals places of the token.
+    function decimals() public view override returns (uint8) {
+        return 18;
+    }
+}
