5987e1379f66cf45fc15edc3c3754b74cf56776e
vile <111662603+vile@users.noreply.github.com>
2024-07-02 18:59:10 -0400

    chore: change NatSpec type

--- test/deploy-gas/SimpleDeployTest.t.sol
+++ test/deploy-gas/SimpleDeployTest.t.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.25;
 import {TestBase, BondERC20} from "../TestBase.sol";
 
 contract SimpleDeployTest is TestBase {
-    /// @notice Deploy a beacon proxy for BondERC20.
+    /// @dev Deploy a beacon proxy for BondERC20.
     function test_proxyDeployment() public {
         vm.prank(USER_ONE);
         proxyFactory.deployNewBondERC20Proxy(
@@ -18,8 +18,8 @@ contract SimpleDeployTest is TestBase {
         );
     }
 
-    /// @notice Deploy the raw implementation contract.
-    /// @notice This is unrealistic, but used for gas comparisons.
+    /// @dev Deploy the raw implementation contract.
+    /// @dev This is unrealistic, but used for gas comparisons.
     function test_rawDeployment() public {
         vm.startPrank(USER_ONE);
         BondERC20 bondInstance = new BondERC20();
